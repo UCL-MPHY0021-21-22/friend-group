c951807b542428cc96bb013fb61adf4c24567c28
congzheng <zczlzco@ucl.ac.uk>
2021-10-21 11:38:15 +0100

    Show the relations between everyone.

--- group.py
+++ group.py
@@ -1,5 +1,32 @@
-"""An example of how to represent a group of acquaintances in Python."""
-
-# Your code to go here...
-
-my_group =
+my_group = {
+    "Jill": {
+        "age": 26,
+        "job": "biologist",
+        "relations": {
+            "Zalika": "friend",
+            "John": "partner"
+        }
+    },
+    "Zalika": {
+        "age": 28,
+        "job": "artist",
+        "relations": {
+            "Jill": "friend"
+        }
+    },
+    "John": {
+        "age": 27,
+        "job": "writer",
+        "relations": {
+            "Jill": "partner"
+        }
+    },
+    "Nash": {
+        "age": 34,
+        "job": "chef",
+        "relations": {
+            "John": "cousin",
+            "Zalika": "landlord"
+        }
+    }
+}
