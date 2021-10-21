894e303889dbff2eb8e35a5bb50eb0ddf1038a06
zyxxyz2021 <zyxdwangyi@163.com>
2021-10-21 12:36:57 +0100

    relationship_net

--- group.py
+++ group.py
@@ -2,4 +2,30 @@
 
 # Your code to go here...
 
-my_group =
+my_group = {
+    'Jill':{
+        'name':'Jill',
+        'age':'26',
+        'occupation':'biologist',
+        'connection':{'friend':['Zalika'],'partner':['John'],'landlord':None,'relatives':None}
+    },
+    'Zalika':{
+        'name':'Zalika',
+        'age':'28',
+        'occupation':'artist',
+        'connection':{'friend':['Jill'],'partner':None,'landlord':['Nash'],'relatives':None}
+    },
+    'John':{
+        'name':'John',
+        'age' :'27',
+        'occupation':'writer',
+        'connection':{'friend':None,'partner':['Jill'],'landlord':None,'relatives':['nash(cousin)']}
+    },
+    'Nash':{
+        'name':'Nash',
+        'age':'34',
+        'occupation':'chef',
+        'connection':{'friend':None,'partner':None,'landlord':None,'relatives':['John(cousin)']}
+    },
+}
+
