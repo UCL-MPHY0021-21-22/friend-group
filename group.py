ca0df9202c2e479c255323f538934b69909198bd
Pete Jones <peterjones252@gmail.com>
2021-10-22 00:30:32 +0100

    Added required functions

--- group.py
+++ group.py
@@ -13,20 +13,43 @@ class person:
     def relationship(self, person2, relationship):
         self.relations[person2] = relationship
 
+def max_age(group):
+    ### Takes an input of a list of names in the class "person" and outputs the maximum age in the group ###
+    return max([name.age for name in group])
 
+def mean_age(group):
+    ### Takes an input of a list of names in the class "person" and outputs the mean age in the group ###
+    return sum([name.age for name in group])/len(group)
 
-Jill = person('Jill','26','Biologist')
-Zalika = person('Zalika','28','Artist')
-John = person('John','27','Writer')
-Nash = person('Nash','34','Chef')
+def max_age_1_relation(group):
+    ### Takes an input of a list of names in the class "person" and outputs the max age in the group if they have at least one relation ###
+    return max([name.age for name in group if len(name.relations) > 0])
 
-Jill.relationship(Zalika,'friend')
-Jill.relationship(John,'partner')
-Zalika.relationship(Jill,'friend')
-John.relationship(Jill,'partner')
-Nash.relationship(John,'cousin')
-Nash.relationship(Zalika,'landlord')
+def max_age_1_friend(group):
+    ### Takes an input of a list of names in the class "person" and outputs the max age in the group if they have at least one friend ###
+    names = []
+    for name in group:
+        for entry in name.relations.values():
+            if entry == 'friend' or entry == 'Friend':
+                names.append(name)
+    return max([name.age for name in names])
 
-my_group = [Jill,Zalika,John,Nash]
+Jill = person('Jill',26,'Biologist')
+Zalika = person('Zalika',28,'Artist')
+John = person('John',27,'Writer')
+Nash = person('Nash',34,'Chef')
+Pete = person('Pete', 52, 'Student')
 
+Jill.relationship('Zalika','friend')
+Jill.relationship('John','partner')
+Zalika.relationship('Jill','friend')
+John.relationship('Jill','partner')
+Nash.relationship('John','cousin')
+Nash.relationship('Zalika','landlord')
 
+my_group = [Jill,Zalika,John,Nash,Pete]
+
+print(f"Max age: " + str(max_age(my_group)))
+print(f"Mean age: " + str(mean_age(my_group)))
+print(f"Max age of those that have one or more relations: " + str(max_age_1_relation(my_group)))
+print(f"Max age of those that have one or more friends: " + str(max_age_1_friend(my_group)))
