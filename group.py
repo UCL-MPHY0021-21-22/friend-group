667a98a55ea12f00e94ec1a3cd7daa1bec807936
DannyChunIWu <zceecwu@ucl.ac.uk>
2021-10-21 11:22:14 +0100

    Added connections (Dict in dict data structure)

--- group.py
+++ group.py
@@ -1,5 +1,42 @@
 """An example of how to represent a group of acquaintances in Python."""
 
 # Your code to go here...
-
-my_group =
+group_info_dic = {
+    'Danny': {
+        'age':25,
+        'job': 'Machine learning engineer',
+        'connection': {
+            "Juncai": 'Classmate',
+            "Jenny": 'Classmate',
+            "Cheng Yuan": 'Classmate'
+        }
+    },
+    'Jenny': {
+        'age': 21,
+        'job': 'Student',
+        'connection': {
+            "Juncai": 'Classmate',
+            "Danny": 'Classmate',
+            "Cheng Yuan": 'Classmate'
+        }
+    },
+    'Juncai': {
+        'age': 22,
+        'job': 'Student',
+        'connection': {
+            "Danny": "Classmate",
+            "Jenny": 'Classmate',
+            "Cheng Yuan": 'Classmate'
+        }
+    },
+    'Cheng Yuan': {
+        'age': 22,
+        'job': 'Student',
+        'connection': {
+            "Danny": "Classmate",
+            "Jenny": 'Classmate',
+            "Juncai": 'Classmate'
+        }
+    }
+}
+my_group = group_info_dic
