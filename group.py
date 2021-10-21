fa8d56a8caa41f3fe14a75967bfcee774d5306fd
CoreyFulop <corey.fulop@gmail.com>
2021-10-21 23:12:06 +0100

    added some code with comprehensions

--- group.py
+++ group.py
@@ -2,43 +2,60 @@
 
 # Your code to go here...
 
-my_group = {"Jill": {
+my_group = {"jill": {
                     "age": 26,
                     "occupation": "biologist",
-                    "friend": "Zalika",
-                    "partner": "John",
+                    "friend": "zalika",
+                    "partner": "john",
                     "cousin": "",
                     "landlord": "",
                     "tenant": ""
                                 }, 
-           "Zalika": {
+           "zalika": {
                     "age": 28,
                     "occupation": "artist",
-                    "relationships": "",
-                    "friend": "",
+                    "friend": "jill",
                     "partner": "",
                     "cousin": "",
-                    "landlord": "",
+                    "landlord": "nash",
                     "tenant": ""
-           }
-           "John": {
+                     },
+           "john": {
                     "age": 27,
                     "occupation": "writer",
-                    "relationships": "",
                     "friend": "",
-                    "partner": "",
-                    "cousin": "",
+                    "partner": "jill",
+                    "cousin": "nash",
                     "landlord": "",
                     "tenant": ""
-           }
-           "Nash": {
+                     },
+           "nash": {
                     "age": 34,
                     "occupation": "chef",
-                    "relationships": "",
                     "friend": "",
                     "partner": "",
-                    "cousin": "",
+                    "cousin": "john",
                     "landlord": "",
-                    "tenant": ""}
+                    "tenant": "zalika"}
            
-           }
+                    }
+
+
+ages = [my_group[person]["age"] for person in my_group]
+
+# Assuming relations means has partner or cousin
+partners = ["partner" for person in my_group if my_group[person]["partner"]]
+cousins = ["cousin" for person in my_group if my_group[person]["cousin"]]
+
+relations = []
+relations.extend(partners + cousins)
+
+is_related = [my_group[person]["age"] for person in my_group if my_group[person]["partner"] or my_group[person]["cousin"]]
+
+has_friends = [my_group[person]["age"] for person in my_group if my_group[person]["friend"]]
+
+# Print results
+print(f"The maximum age in our group is: {max(ages)}")
+print(f"The average number of relations among members of the group is: {len(relations)//len(my_group)}")
+print(f"The maximum age of the person that has at least one relation is: {max(is_related)}")
+print(f"The maximum age of the person that has at least one friend is: {max(has_friends)}")
