4d78414fbb0e52f2f37bc56ec3ca0476126b960c
Elizabeth Guest <elizabeth.guest.21@ucl.ac.uk>
2021-10-21 11:23:25 +0100

    written dictionaries of friends

--- group.py
+++ group.py
@@ -3,5 +3,15 @@
 # Your code to go here...
 
 my_group = {'Jill' : 
-        {'age' = 
+        {'age' : 26 , 'job' : 'biologist' , 'connections' : 
+             {'friend' : 'Zalika' , 'partner' : 'John'}   } , 
+            'Zalika' : 
+        {'age' : 28 , 'job' : 'artist' , 'connections' : 
+             {'friend' : 'Jill' , 'landlord' : 'Nash'}   } , 
+            'John' : 
+        {'age' : 27 , 'job' : 'writer' , 'connections' : 
+             {'partner' : 'Jill' , 'cousin' : 'Nash'}   } , 
+            'Nash' : 
+        {'age' : 34 , 'job' : 'chef' , 'connections' : 
+             {'cousin' : 'John' , 'tenant' : 'Zalika'}   }   }
 
