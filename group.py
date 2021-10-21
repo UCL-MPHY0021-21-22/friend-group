82fb3e4d4191529430569d33afea6e5123e875cb
Thomas <thomasclarke28@hotmail.co.uk>
2021-10-21 11:38:40 +0100

    git commit -m "Started work on building classes"

--- group.py
+++ group.py
@@ -2,4 +2,20 @@
 
 # Your code to go here...
 
-my_group =
+my_group = Jiayi21-JiefuMei-myx2021-thandi1908-SolemnShark871
+
+class Person:
+    def _init_(self, name, age, occupation ):
+        self.name = name
+        self.age = age
+        self.occupation = occupation
+        self.relationships = {}  #define relationship
+
+    def add_relationships(self, name, relationships):
+        
+
+
+Jill_relations = {Zalika: ""friend", John: "partner"}
+    
+
+    
