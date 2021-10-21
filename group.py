55ec3afac8b383e23602231eb0ec31361d5ff684
Hasan Sayginel <hasan.sayginel.17@ucl.ac.uk>
2021-10-21 11:38:17 +0100

    create dict for a friendship group

--- group.py
+++ group.py
@@ -2,4 +2,43 @@
 
 # Your code to go here...
 
-my_group =
+my_group = {"Jill": {
+                    "age": 26,
+                    "occupation": "biologist",
+                    "friend": "Zalika",
+                    "partner": "John",
+                    "cousin": "",
+                    "landlord": "",
+                    "tenant": ""
+                                }, 
+           "Zalika": {
+                    "age": 28,
+                    "occupation": "artist",
+                    "relationships": "",
+                    "friend": "",
+                    "partner": "",
+                    "cousin": "",
+                    "landlord": "",
+                    "tenant": ""
+           }
+           "John": {
+                    "age": 27,
+                    "occupation": "writer",
+                    "relationships": "",
+                    "friend": "",
+                    "partner": "",
+                    "cousin": "",
+                    "landlord": "",
+                    "tenant": ""
+           }
+           "Nash": {
+                    "age": 34,
+                    "occupation": "chef",
+                    "relationships": "",
+                    "friend": "",
+                    "partner": "",
+                    "cousin": "",
+                    "landlord": "",
+                    "tenant": ""}
+           
+           }
