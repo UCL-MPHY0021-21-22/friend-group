c31491988660fab2cbacc2d8ea0eeb68bf232d4e
kjhmh2 <1492012973@qq.com>
2021-10-21 12:04:27 +0100

    add data functions

--- group.py
+++ group.py
@@ -29,6 +29,30 @@ my_group = {
     }
 }
 
+def forget(person1, person2):
+    for name, features in my_group.items():
+        if name == person1:
+            del features['connection'][person2]
+        if name == person2:
+            del features['connection'][person1]
+
+def add_person(name, age, job, relations):
+    new_features = {'age': age, 'job': job, 'connection': relations}
+    my_group[name] = new_features
+
+def average_age():
+    cnt = 0
+    for name, features in my_group.items():
+        cnt += features['age']
+    return cnt / len(my_group)
+
+forget('Jill', 'Zalika')
+add_person("Jack", 20, 'teacher', {'Zalika': 'landlord'})
+
+print(my_group)
+print(average_age())
+
+
 # Questions:
 # 1 Yes
 # 2 Yes
