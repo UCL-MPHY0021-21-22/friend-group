bed222e4f6528e5f7d07b63ddbc4a35d565a4933
Shashvat Shukla <shashvatshukla@gmail.com>
2021-10-22 23:59:33 +0100

    Add functions to get summary data from group

--- group.py
+++ group.py
@@ -2,6 +2,8 @@
 
 # Your code to go here...
 
+from statistics import mean
+
 my_group = {
         "Jill": { "age": 26,
                 "jobs": ["Biologist"],
@@ -12,23 +14,41 @@ my_group = {
                 },
             "Zalika": {
                 "age": 28,
-                "jobs": ["Artist"]},
+                "jobs": ["Artist"],
                 "connections": {
                     "friend": ["Jill"]
                     }
+            },
+            
             "John": {
                 "age": 27,
-                "jobs": ["Writer"]},
+                "jobs": ["Writer"],
                 "connections": {
                     "partner": ["Jill"]
                     }
+            },
             "Nash": {
                 "age": 34,
                 "jobs": ["Chef"],
                 "connections": {
-                    {
                     "cousin": ["John"],
-                    "landlord": ["Zalika"]
-                    }
+                    "landlord": ["Zalika"]   
                 }
-        }
+           }
+}
+
+
+def max_age(group):
+    return max([person["age"] for person in list(group.values())])
+
+def num_connections(person):
+    return len(person["connections"].keys())
+
+def average_degree(group):
+    return mean([num_connections(person) for person in list(group.values())])
+
+def max_age_with_one_relation(group):
+    return max([person["age"] for person in list(group.values()) if num_connections(person)])
+
+def max_age_with_one_friend(group):
+    return max([person["age"] for person in list(group.values()) if person["connections"].get("friend")])
