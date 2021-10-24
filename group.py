4d2648d98ac0e994bc352106f851dbcff149bb00
Pete Jones <peterjones252@gmail.com>
2021-10-24 17:16:22 +0100

    Made suggested changes

--- group.py
+++ group.py
@@ -14,19 +14,19 @@ class person:
         self.relations[person2] = relationship
 
 def max_age(group):
-    ### Takes an input of a list of names in the class "person" and outputs the maximum age in the group ###
+    """ Takes an input of a list of names in the class "person" and outputs the maximum age in the group """
     return max([name.age for name in group])
 
 def mean_age(group):
-    ### Takes an input of a list of names in the class "person" and outputs the mean age in the group ###
+    """ Takes an input of a list of names in the class "person" and outputs the mean age in the group """
     return sum([name.age for name in group])/len(group)
 
 def max_age_1_relation(group):
-    ### Takes an input of a list of names in the class "person" and outputs the max age in the group if they have at least one relation ###
-    return max([name.age for name in group if len(name.relations) > 0])
+    """ Takes an input of a list of names in the class "person" and outputs the max age in the group if they have at least one relation """
+    return max([name.age for name in group if name.relations])
 
 def max_age_1_friend(group):
-    ### Takes an input of a list of names in the class "person" and outputs the max age in the group if they have at least one friend ###
+    """ Takes an input of a list of names in the class "person" and outputs the max age in the group if they have at least one friend """
     return max([name.age for name in group if list(name.relations.values()).count("friend") >=1])
 
 Jill = person('Jill',26,'Biologist')
@@ -44,7 +44,7 @@ Nash.relationship('Zalika','landlord')
 
 my_group = [Jill,Zalika,John,Nash,Pete]
 
-print(f"Max age: " + str(max_age(my_group)))
-print(f"Mean age: " + str(mean_age(my_group)))
-print(f"Max age of those that have one or more relations: " + str(max_age_1_relation(my_group)))
-print(f"Max age of those that have one or more friends: " + str(max_age_1_friend(my_group)))
+print(f"Max age: {max_age(my_group)}")
+print(f"Mean age: {mean_age(my_group)}")
+print(f"Max age of those that have one or more relations: {max_age_1_relation(my_group)}")
+print(f"Max age of those that have one or more friends: {max_age_1_friend(my_group)}")
