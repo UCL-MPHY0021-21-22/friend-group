c99f39bfe56e5d97bea488c59b228cb429db96a8
Abs Arosi Surface <absarosi@gmail.com>
2021-10-25 15:14:54 +0100

    max age comprehension added

--- group.py
+++ group.py
@@ -1,5 +1,43 @@
-"""An example of how to represent a group of acquaintances in Python."""
+group = {
+    "Jill": {
+        "age": 26,
+        "job": "biologist",
+        "relations": {
+            "Zalika": "friend",
+            "John": "partner"
+        }
+    },
+    "Zalika": {
+        "age": 28,
+        "job": "artist",
+        "relations": {
+            "Jill": "friend"
+        }
+    },
+    "John": {
+        "age": 27,
+        "job": "writer",
+        "relations": {
+            "Jill": "partner"
+        }
+    },
+    "Nash": {
+        "age": 34,
+        "job": "chef",
+        "relations": {
+            "John": "cousin",
+            "Zalika": "landlord"
+        }
+    }
+}
 
-# Your code to go here...
 
-my_group =
+# Add some code that makes use of comprehension expressions to your group.py file so that it prints out the following when the script is run:
+
+# the maximum age of people in the group
+max_age = max([group.get(personName).get("age") for personName in group.keys()])
+
+
+# the average (mean) number of relations among members of the group
+# the maximum age of people in the group that have at least one relation
+# [more advanced] the maximum age of people in the group that have at least one friend
