5575a52124c32bca02cbccfeff59ac3393c083b8
Abs Arosi Surface <absarosi@gmail.com>
2021-10-25 17:20:31 +0100

    Maximum age given at least one relation

--- group.py
+++ group.py
@@ -37,17 +37,19 @@ group = {
     }
 }
 
-
-
+# the maximum age of people in the group
 max_age = max([group.get(personName).get("age") for personName in group.keys()])
 print("oldest person is", max_age)
 
-
+# the average (mean) number of relations among members of the group
 import numpy as np
 relationships_dict_list = [group.get(personName).get("relations") for personName in group.keys()]
 mean_relations = np.mean([len(personalRelations.keys()) for personalRelations in relationships_dict_list])
-print("mean relations is ", mean_relations)
+print("mean number of relations is", mean_relations)
 
+# the maximum age of people in the group that have at least one relation
+max_nonfriendless = max([group.get(personName).get("age") for personName in group.keys() if len(group.get(personName).get("relations")) != 0])
+print("oldest person with at least one friend is", max_nonfriendless, "years old")
 
 
 # [more advanced] the maximum age of people in the group that have at least one friend
