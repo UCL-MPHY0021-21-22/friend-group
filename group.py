2b6f845289ce16f8c5deaa7161f0f4e9066f9973
ezubs <liza_zubanova@yahoo.co.uk>
2021-10-27 09:07:47 +0100

    in_class_ex_solution

--- group.py
+++ group.py
@@ -2,4 +2,35 @@
 
 # Your code to go here...
 
-my_group =
+my_group = {
+    "Jill": {
+        "age": 26,
+        "job": "biologist",
+        "relations": {
+            "Zalika": "friend",
+            "John": "partner"
+        }
+    },
+    "Zalika": {
+        "age": 28,
+        "job": "artist",
+        "relations": {
+            "Jill": "friend"
+        }
+    },
+    "John": {
+        "age": 27,
+        "job": "writer",
+        "relations": {
+            "Jill": "partner"
+        }
+    },
+    "Nash": {
+        "age": 34,
+        "job": "chef",
+        "relations": {
+            "John": "cousin",
+            "Zalika": "landlord"
+        }
+    }
+}
