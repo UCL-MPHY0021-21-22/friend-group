83dc2c38c600c15af508d8112a365da81e1c4231
Jenny Zhou <zceiyyz@ucl.ac.uk>
2021-10-27 16:15:11 +0100

    Exercise done using dictionaries

--- group.py
+++ group.py
@@ -2,4 +2,17 @@
 
 # Your code to go here...
 
-my_group =
+my_group = {
+    "Jill" : {"age" : 26, "job" : "biologist", 
+            "connections" : {"Zalika" : "friend", "John":"partner"}},
+    "Zalika" : {"age": 28, "job": "artist",
+            "connections" : {"Jill":"friend"}},
+    "John" : {"age":27, "job": "writer", "connections":{"Jill":"partner"}},
+    "Nash":{"age":34, "job":"chef", "connections":{"John":"cousin", "Zalika":"landlord"}}
+}
+
+
+- Jill is 26, a biologist and she is Zalika's friend and John's partner.
+- Zalika is 28, an artist, and Jill's friend
+- John is 27, a writer, and Jill's partner.
+- Nash is 34, a chef, John's cousin and Zalika's landlord.
