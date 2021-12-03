fc2f097ff8a9eada02d09b5223b968b48edded0c
Qiyuan Zeng <zczlq21@ucl.ac.uk>
2021-12-03 12:08:10 +0000

    Complete tasks-Summary data from group

--- group.py
+++ group.py
@@ -37,3 +37,24 @@ my_group = {
         }
     }
 }
+
+
+# 3.1
+# the maximum age of people in the group
+max_age = max([properties['age'] for properties in my_group.values()])
+print(f"Max age: {max_age}")
+
+# 3.2 
+# the average (mean) number of relations among members of the group
+mean_number_of_relations = sum([len(properties['relations']) for properties in my_group.values()]) / len(my_group)
+print(f"mean # of relations: {mean_number_of_relations}")
+
+# 3.3 
+# the maximum age of people in the group that have at least one relation
+age_of_people_with_relations = [properties['age'] for properties in my_group.values() if len(properties['relations']) >=1]
+print(f"Max age, with relations: {max(age_of_people_with_relations)}")
+
+# 3.4 
+# the maximum age of people in the group that have at least one friend
+age_of_people_with_friend = [properties['age'] for properties in my_group.values() if 'friend' in properties['relations'].values()]
+print(f"Max age, with friend: {age_of_people_with_friend}")
